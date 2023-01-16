c615600fdd08a8a7724d479b00dee804748b4fee
Matthew Eskolin <matteskolin@gmail.com>
2023-01-16 13:24:21 -0600

    blazor

---
+++ BlazingTrails/BlazingTrails.Client/Features/Home/Trail.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazingTrails.Client.Features.Home;
+
+public class Trail
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Image { get; set; } = "";
+    public string Location { get; set; } = "";
+    public int TimeInMinutes { get; set; }
+    public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+    public int Length { get; set; }
+    public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
+}
+
+public class RouteInstruction
+{
+    public int Stage { get; set; }
+    public string Description { get; set; } = "";
+}
