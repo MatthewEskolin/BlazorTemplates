ffd9aadaf4e2e05177113a9571fdc165ee6a0a28
Matthew Eskolin <matteskolin@gmail.com>
2023-02-15 15:56:08 -0600

    fix build

--- BlazingTrails.Api/Program.cs
+++ BlazingTrails.Api/Program.cs
@@ -36,34 +36,3 @@ app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.Run();
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddDbContext<BlazingTrailsContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("BlazingTrailsContext")));
-builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.Load("BlazingTrails.Shared")));
-
-
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
