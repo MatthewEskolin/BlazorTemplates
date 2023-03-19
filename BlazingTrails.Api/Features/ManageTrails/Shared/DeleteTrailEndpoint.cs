17b130ef9067cbfefd9f62465265ff607098edae
Matthew Eskolin <matteskolin@gmail.com>
2023-03-19 08:46:44 -0500

    cleanup

---
+++ BlazingTrails.Api/Features/ManageTrails/Shared/DeleteTrailEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using Ardalis.ApiEndpoints;
+using BlazingTrails.Api.Persistence;
+using BlazingTrails.Shared.Features.Home.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingTrails.Api.Features.ManageTrails.Shared;
+
+public class DeleteTrailEndpoint : EndpointBaseAsync.WithRequest<DeleteTrailRequest>.WithActionResult<bool>
+{
+    private readonly BlazingTrailsContext _database;
+
+    public DeleteTrailEndpoint(BlazingTrailsContext database)
+    {
+        _database = database;
+    }
+
+    [HttpDelete(DeleteTrailRequest.RouteTemplate)]
+    public override async Task<ActionResult<bool>> HandleAsync(DeleteTrailRequest request, CancellationToken cancellationToken = default)
+    {
+        var trail = await _database.Trails.Include(x => x.Route).SingleOrDefaultAsync(x => x.Id == request.DeleteTrail.TrailId, cancellationToken: cancellationToken);
+
+        if (trail is null)
+        {
+            return BadRequest("Trail could not be found.");
+        }
+
+        //Delete Routes, then delete Trail
+        _database.RouteInstructions.RemoveRange(trail.Route);
+        _database.Trails.Remove(trail);
+
+        //remove image
+        if (trail.Image != null)
+        {
+            //delete it
+            System.IO.File.Delete(Path.Combine(Directory.GetCurrentDirectory(), "Images", trail.Image!));
+        }
+
+        await _database.SaveChangesAsync(cancellationToken);
+
+        return Ok(true);
+    }
+}
--- BlazingTrails.Api/Features/ManageTrails/Shared/UploadTrailImageEndpoint.cs
+++ BlazingTrails.Api/Features/ManageTrails/Shared/UploadTrailImageEndpoint.cs
@@ -1,6 +1,5 @@
 ﻿using Ardalis.ApiEndpoints;
 using BlazingTrails.Api.Persistence;
-using BlazingTrails.Shared.Features.Home.Shared;
 using BlazingTrails.Shared.Features.ManageTrails.EditTrail;
 using BlazingTrails.Shared.Features.ManageTrails;
 using BlazingTrails.Shared.Features.ManageTrails.AddTrail;
@@ -58,43 +57,5 @@ public class UploadTrailImageEndpoint : EndpointBaseAsync.WithRequest<int>.WithA
         await _database.SaveChangesAsync(cancellationToken);
 
         return Ok(trail.Image);
-    }
-}
-
-
-
-public class DeleteTrailEndpoint : EndpointBaseAsync.WithRequest<DeleteTrailRequest>.WithActionResult<bool>
-{
-    private readonly BlazingTrailsContext _database;
-
-    public DeleteTrailEndpoint(BlazingTrailsContext database)
-    {
-        _database = database;
-    }
-
-    [HttpDelete(DeleteTrailRequest.RouteTemplate)]
-    public override async Task<ActionResult<bool>> HandleAsync(DeleteTrailRequest request, CancellationToken cancellationToken = default)
-    {
-        var trail = await _database.Trails.Include(x => x.Route).SingleOrDefaultAsync(x => x.Id == request.DeleteTrail.TrailId, cancellationToken: cancellationToken);
-
-        if (trail is null)
-        {
-            return BadRequest("Trail could not be found.");
-        }
-
-        //Delete Routes, then delete Trail
-        _database.RouteInstructions.RemoveRange(trail.Route);
-        _database.Trails.Remove(trail);
-
-        //remove image
-        if (trail.Image != null)
-        {
-            //delete it
-            System.IO.File.Delete(Path.Combine(Directory.GetCurrentDirectory(), "Images", trail.Image!));
-        }
-
-        await _database.SaveChangesAsync(cancellationToken);
-
-        return Ok(true);
     }
 }
