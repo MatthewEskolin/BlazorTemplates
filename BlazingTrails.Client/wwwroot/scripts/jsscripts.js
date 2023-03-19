efdb853a01191b0e8d65d84abb5ee702638e70ba
Matthew Eskolin <matteskolin@gmail.com>
2023-03-18 21:17:51 -0500

    scroll to top

---
+++ BlazingTrails.Client/wwwroot/scripts/jsscripts.js
@@ -0,0 +1,11 @@
+    window.alertTest = function() {
+        alert('jstest');
+    }
+
+    window.backToTop = function(){
+        document.getElementsByClassName('content')[0].scrollTop = 0;
+}
+
+function testalert() {
+    alert('testalert');
+}
